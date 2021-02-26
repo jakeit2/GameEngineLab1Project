0c7e48106312a9fcc1e7adc8661ca196019ac64d
sheidlja <70345692+sheidlja@users.noreply.github.com>
2021-02-26 17:19:44 -0500

    Add files via upload

---
+++ Game/Assets/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform FelixTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - FelixTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = FelixTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+    }
+}
