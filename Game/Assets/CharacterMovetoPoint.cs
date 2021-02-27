630398a10c940ed061db03a92fe4f8c9cdd66e4a
jakeit2 <donawesome8@gmail.com>
2021-02-27 18:50:22 -0500

    Cinemachine

---
+++ Game/Assets/CharacterMovetoPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovetoPoint : MonoBehaviour
+{
+    public Transform objectLocation;
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 movement;
+    private Animator ant;
+
+    void Start(){
+        rb = this.GetComponent<Rigidbody>();
+        ant = this.GetComponent<Animator>();
+    }
+
+    void Update(){
+        Vector3 direction = objectLocation.position - transform.position;
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate(){
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector3 direction){
+        rb.MovePosition(transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
