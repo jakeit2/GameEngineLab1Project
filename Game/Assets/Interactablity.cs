61ce2a227af3a7beeadd7605161bc7be0bf05674
jakeit2 <donawesome8@gmail.com>
2021-03-13 16:29:32 -0500

    commit

--- Game/Assets/Interactablity.cs
+++ Game/Assets/Interactablity.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Interactablity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject triggeringNPC;
+    private bool triggering;
 
+    public GameObject npcText;
     // Update is called once per frame
     void Update()
     {
-        
+        if(triggering)
+        {
+            npcText.SetActive(true);
+            print("Player is triggering with " + triggeringNPC);
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                triggering = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "NPC")
+        {
+            triggering = true;
+            triggeringNPC = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "NPC")
+        {
+            triggering = false;
+            triggeringNPC = null;
+        }
     }
 }
