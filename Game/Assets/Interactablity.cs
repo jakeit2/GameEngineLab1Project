7d230a10e2fab8ab3ab66c3e315a275c1fc34ffb
Donald Hutchinson <donawesome8@gmail.com>
2021-03-14 19:09:20 -0400

    Dialogue

--- Game/Assets/Interactablity.cs
+++ Game/Assets/Interactablity.cs
@@ -7,18 +7,28 @@ public class Interactablity : MonoBehaviour
     private GameObject triggeringNPC;
     private bool triggering;
 
+    public GameObject dialogueText;
     public GameObject npcText;
     // Update is called once per frame
+
     void Update()
     {
+        dialogueText.SetActive(true);
         if(triggering)
         {
+            
             npcText.SetActive(true);
-            print("Player is triggering with " + triggeringNPC);
-            if(Input.GetKeyDown(KeyCode.E))
+            //print("Player is triggering with " + triggeringNPC);
+            if(Input.GetKeyDown(KeyCode.F))
             {
+
                 triggering = false;
+                npcText.SetActive(false);
+                dialogueText.SetActive(true);
             }
+        }else {
+            npcText.SetActive(false);
+            
         }
     }
 
---
+++ Game/Assets/TimelineZone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Events;
+
+public class TimelineZone : MonoBehaviour
+{
+    public enum TriggerType
+    {
+        Once, Everytime,
+    }
+
+    public GameObject triggeringGameObject;
+    public PlayableDirector director;
+    public TriggerType triggerType;
+    public UnityEvent OnDirectorPlay;
+    public UnityEvent OnDirectorFinish;
+
+    protected bool m_AleardyTriggered;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject != triggeringGameObject)
+        return;
+
+        if(triggerType == TriggerType.Once && m_AleardyTriggered)
+        return;
+
+        OnDirectorPlay.Invoke();
+        director.Play();
+        m_AleardyTriggered = true;
+        Invoke("FinishInvoke", (float)director.duration);
+    }
+
+    void FinishInvoke()
+    {
+        OnDirectorFinish.Invoke();
+    }
+}
